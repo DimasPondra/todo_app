a0503304e4cc70e59de45cfef03e6feed98b312e
Dimas Pondra Oktafianto <77646429+DimasPondra@users.noreply.github.com>
2023-10-11 08:17:43 +0700

    :sparkles: show data from rest API

--- lib/screens/todo_list.dart
+++ lib/screens/todo_list.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:todo_app/screens/add_page.dart';
+import 'package:http/http.dart' as http;
 
 class TodoListPage extends StatefulWidget {
   const TodoListPage({Key? key}) : super(key: key);
@@ -9,11 +12,43 @@ class TodoListPage extends StatefulWidget {
 }
 
 class _TodoListPageState extends State<TodoListPage> {
+  bool isLoading = true;
+  List items = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchTodo();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Todo List'),
+      ),
+      body: Visibility(
+        visible: isLoading,
+        child: const Center(child: CircularProgressIndicator()),
+        replacement: RefreshIndicator(
+          onRefresh: fetchTodo,
+          child: ListView.builder(
+              itemCount: items.length,
+              itemBuilder: (context, index) {
+                final item = items[index] as Map;
+                return ListTile(
+                  leading: CircleAvatar(child: Text('${index + 1}')),
+                  title: Text(item['title']),
+                  subtitle: Text(item['description']),
+                  trailing: PopupMenuButton(itemBuilder: (context) {
+                    return [
+                      const PopupMenuItem(child: Text('edit')),
+                      const PopupMenuItem(child: Text('delete')),
+                    ];
+                  }),
+                );
+              }),
+        ),
       ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: navigateToAddPage,
@@ -27,5 +62,23 @@ class _TodoListPageState extends State<TodoListPage> {
       builder: (context) => const AddTodoPage(),
     );
     Navigator.push(context, route);
+  }
+
+  Future<void> fetchTodo() async {
+    final response = await http.get(
+      Uri.parse('https://todo-api.dimasoktafianto.my.id/api/todos'),
+    );
+
+    if (response.statusCode == 200) {
+      final json = jsonDecode(response.body) as Map;
+      final result = json['data'] as List;
+      setState(() {
+        items = result;
+      });
+    }
+
+    setState(() {
+      isLoading = false;
+    });
   }
 }
